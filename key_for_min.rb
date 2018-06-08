6f9e531380518403bae169ad03710cc084e70253
Marie Cain <mariecain27@gmail.com>
2018-06-08 01:29:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,3 +10,14 @@ def key_for_min_value(name_hash)
  smallest
 end
 
+def key_for_min_value(hash)
+  smallest_key = nil
+  tiniest_value = nil
+  hash.each do |key, value|
+    if tiniest_value == nil || value < tiniest_value
+      tiniest_value = value
+      smallest_key = key
+    end
+  end
+  smallest_key
+end
